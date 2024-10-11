024220460ff39fe058d62f7480ed17fcf1530a22
AbbieMutzie <165520840+AbbieMutzie@users.noreply.github.com>
2024-10-11 13:08:28 +0300

    Added booking frontend

--- booking/views.py
+++ booking/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from .models import Machinery, Booking
 from .serializers import MachinerySerializer, BookingSerializer
 
+
 class MachineryViewSet(viewsets.ModelViewSet):
     queryset = Machinery.objects.all()
     serializer_class = MachinerySerializer
@@ -9,3 +10,4 @@ class MachineryViewSet(viewsets.ModelViewSet):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
